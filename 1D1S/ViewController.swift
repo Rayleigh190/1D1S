4a3819b600617fd5406e20b55a6bf4226aa5a4b5
rayleigh <rayleigh190@gmail.com>
2025-01-31 20:18:18 +0900

    Initial Commit

---
+++ 1D1S/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1D1S
+//
+//  Created by 우진 on 1/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
